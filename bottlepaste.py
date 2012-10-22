a39a45e85bc17831196a8027ef56cc6f7e61e19e
Valentin Haenel <valentin.haenel@gmx.de>
2012-10-22 14:47:08 +0200

    add code to post code

--- bottlepaste.py
+++ bottlepaste.py
@@ -1,8 +1,14 @@
-from bottle import route, run
+from bottle import route, run, request
 
 
 @route('/')
 def index():
     return '<b>Hello World</b>!'
+
+
+@route('/', method='POST')
+def upload():
+    code = request.forms.get("bp")
+    return code
 
 run(host='localhost', port=8080)

cc3372dd7a6b4a7e24ee67987f2ec6c0444864eb
Valentin Haenel <valentin.haenel@gmx.de>
2012-10-22 15:00:03 +0200

    return a proper url when posting

--- bottlepaste.py
+++ bottlepaste.py
@@ -2,6 +2,7 @@ import hashlib
 from bottle import route, run, request
 
 storage = {}
+BASE_URL='http://localhost:8080/'
 
 def hash(str_):
     return hashlib.sha224(str_).hexdigest()
@@ -17,7 +18,8 @@ def show(uid):
 @route('/', method='POST')
 def upload():
     code = request.forms.get("bp")
-    digest = hash(code)
+    digest = hash(code)[:7]
     storage[digest] = code
+    return "http://localhost:8080/%s\n" % digest
 
 run(host='localhost', port=8080)

9b2e73555ea225edf99767a727ec72af14d7cc5d
Valentin Haenel <valentin.haenel@gmx.de>
2012-10-22 15:04:50 +0200

    added description

--- bottlepaste.py
+++ bottlepaste.py
@@ -4,12 +4,20 @@ from bottle import route, run, request
 storage = {}
 BASE_URL='http://localhost:8080/'
 
+DESCRIPTION = """
+NAME
+    sprunge: command line pastebin.
+
+SYNOPSIS
+    <command> | curl -F 'sprunge=<-' %s
+""" % BASE_URL
+
 def hash(str_):
     return hashlib.sha224(str_).hexdigest()
 
 @route('/')
 def index():
-    return "</br>".join(storage.keys())
+    return DESCRIPTION
 
 @route('/<uid>')
 def show(uid):

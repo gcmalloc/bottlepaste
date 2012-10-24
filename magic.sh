f25da9eddca6a0587ceb41f98c70b7b33017527f
Valentin Haenel <valentin.haenel@gmx.de>
2012-10-24 16:53:46 +0200

    adding magic.sh

---
+++ magic.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+git push github master &&
+git co custom &&
+git rebase master &&
+git push -f github custom  &&
+git push -f cloud custom:master &&
+cctrlapp bottlepaste/default deploy &&
+git co master
